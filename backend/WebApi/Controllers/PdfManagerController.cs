04bba6c19eee687f4a63db9dc2d80dab179cc313
Ankhbayar <antony.baasan@gmail.com>
2017-10-12 20:03:24 -0400

    add file upload method

--- backend/WebApi/Controllers/PdfManagerController.cs
+++ backend/WebApi/Controllers/PdfManagerController.cs
@@ -1,3 +1,8 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PdfManager.Services;
 
@@ -11,6 +16,30 @@ namespace webapi.Controllers
         {
             var pdfMergeService = new PdfMergeService();
             return pdfMergeService.MergeFiles(new string[] { "" });
+        }
+
+        [HttpPost("UploadFiles")]
+        public async Task<IActionResult> UploadAsync(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            // full path to file in temp location
+            var filePath = Path.GetTempFileName();
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            // process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+            return Ok(new { count = files.Count, size, filePath });
         }
 
     }
